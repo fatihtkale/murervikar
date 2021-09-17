1bf184b9c5bc87f550dd7f7858fa1299754778d4
fatih <fatihtkale@gmail.com>
2021-09-17 15:41:59 +0200

    added project files and minor changes

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Nav />
+  <router-view/>
+</template>
+
+<script>
+import Nav from "@/components/Navbar/Navbar.vue"
+
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
